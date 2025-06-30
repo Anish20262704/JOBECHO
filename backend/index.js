08e61e6450fb39c2d3beb5367808db4452076c39
Anish20262704 <shivamanish4971@gmail.com>
2025-06-30 12:35:50 +0530

    frontend env setup

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import serverless from "serverless-http"; // <-- import serverless-http
 
 dotenv.config({});
 
@@ -14,17 +15,18 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
 
+// cors
+const corsOptions = {
+    origin: 'http://localhost:5173',   // local dev pe
+    credentials: true
+};
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
-
+// connect DB once
+connectDB();
 
 // api's
 app.use("/api/v1/user", userRoute);
@@ -32,9 +34,5 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
+// instead of app.listen
+export default serverless(app);
